c980540ab43748b4a690d113cc69a2af11ece3e7
Jesse Chen <jesse.z.chen@gmail.com>
2024-10-18 15:29:48 -0400

    Draw grid and walls

--- frictionless.js
+++ frictionless.js
@@ -1,1 +1,48 @@
-console.log('It works!');
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+});
+  
+const verticalWalls = [[2, 3], [1, 4], [3, 4]];
+const horizontalWalls = [[3, 2], [4, 1], [4, 3]];
+const GRID_SIZE = 8;
+const RESOLUTION = 1024;
+const CELL_SIZE = RESOLUTION / GRID_SIZE;
+const SVG_NS = "http://www.w3.org/2000/svg";
+const players = [[2, 2]];
+
+function init() {
+    const canvas = document.getElementById("content");
+    drawGrid(canvas);
+    drawWalls(canvas);
+}
+
+function drawGrid(canvas) {
+    let gridPath = `M${RESOLUTION} 0H0v${RESOLUTION}`; // outer edge
+    for (let x = 0; x <= GRID_SIZE; x++) {
+        gridPath += `M${x * CELL_SIZE} 0v${RESOLUTION}`; // vertical lines
+    }
+    for (let y = 0; y <= GRID_SIZE; y++) {
+        gridPath += `M0 ${y * CELL_SIZE}h${RESOLUTION}`; // horizontal lines
+    }
+    const pathEl = document.createElementNS(SVG_NS, "path");
+    pathEl.setAttribute("d", gridPath);
+    pathEl.setAttribute("stroke", "#222");
+    pathEl.setAttribute("fill", "none");
+    canvas.appendChild(pathEl);
+}
+
+function drawWalls(canvas) {
+    let wallPath = "";
+    for (let verticalWall of verticalWalls) {
+        wallPath += `M${verticalWall[0] * CELL_SIZE} ${verticalWall[1] * CELL_SIZE}v${CELL_SIZE}`;
+    }
+    for (let horizontalWall of horizontalWalls) {
+        wallPath += `M${horizontalWall[0] * CELL_SIZE} ${horizontalWall[1] * CELL_SIZE}h${CELL_SIZE}`;
+    }
+    const pathEl = document.createElementNS(SVG_NS, "path");
+    pathEl.setAttribute("d", wallPath);
+    pathEl.setAttribute("stroke", "#111");
+    pathEl.setAttribute("stroke-width", "5");
+    pathEl.setAttribute("fill", "none");
+    canvas.appendChild(pathEl);
+}
