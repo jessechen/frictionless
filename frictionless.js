498f03ed0a7cd7238d9259ea611d3fe1b825fbe1
Jesse Chen <jesse.z.chen@gmail.com>
2024-10-18 16:24:35 -0400

    It's aliiive!

--- frictionless.js
+++ frictionless.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", init);
-  
+
+class Friend {
+    constructor(x, y, name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+}
+
 const verticalWalls = [[2, 3], [1, 4], [3, 4]];
 const horizontalWalls = [[3, 2], [4, 1], [4, 3]];
 const GRID_SIZE = 8;
 const RESOLUTION = 1024;
 const CELL_SIZE = RESOLUTION / GRID_SIZE;
 const SVG_NS = "http://www.w3.org/2000/svg";
-let players = [[2, 2]];
+let friends = new Map();
+friends.set("red-friend", new Friend(2, 2, "red-friend"))
 
 function init() {
     const canvas = document.getElementById("content");
@@ -54,9 +63,9 @@ function drawFriends(canvas) {
     imageEl.setAttribute("href", "robot-antennas.svg");
     imageEl.setAttribute("height", CELL_SIZE);
     imageEl.setAttribute("width", CELL_SIZE);
-    imageEl.setAttribute("x", players[0][0] * CELL_SIZE);
-    imageEl.setAttribute("y", players[0][1] * CELL_SIZE);
-    imageEl.setAttribute("id", "red-friend");
+    imageEl.setAttribute("x", friends.get("red-friend").x * CELL_SIZE);
+    imageEl.setAttribute("y", friends.get("red-friend").y * CELL_SIZE);
+    imageEl.setAttribute("id", friends.get("red-friend").name);
     canvas.appendChild(imageEl);
     return imageEl;
 }
@@ -64,19 +73,52 @@ function drawFriends(canvas) {
 function handleKeydown(evt) {
     switch(evt.key) {
         case "ArrowUp":
-            players[0][1] -= 1;
-            break;
         case "ArrowRight":
-            players[0][0] += 1;
-            break;
         case "ArrowDown":
-            players[0][1] += 1;
-            break;
         case "ArrowLeft":
-            players[0][0] -= 1;
+            moveFriend("red-friend", evt.key);
+            const friendEl = document.getElementById(friends.get("red-friend").name);
+            friendEl.setAttribute("x", friends.get("red-friend").x * CELL_SIZE);
+            friendEl.setAttribute("y", friends.get("red-friend").y * CELL_SIZE);
             break;
     }
-    const friendEl = document.getElementById("red-friend");
-    friendEl.setAttribute("x", players[0][0] * CELL_SIZE);
-    friendEl.setAttribute("y", players[0][1] * CELL_SIZE);
+}
+
+function moveFriend(name, direction) {
+    const friend = friends.get(name);
+    let newX, newY;
+    switch(direction) {
+        case "ArrowUp":
+            newY = horizontalWalls.filter((wall) => wall[0] === friend.x)
+                .filter((wall) => wall[1] <= friend.y)
+                .map((wall) => wall[1])
+                .sort()
+                .at(-1);
+            friend.y = newY || 0;
+            break;
+        case "ArrowRight":
+            newX = horizontalWalls.filter((wall) => wall[1] === friend.y)
+                .filter((wall) => wall[1] > friend.x)
+                .map((wall) => wall[1])
+                .sort()
+                .at(0);
+            friend.x = newX || GRID_SIZE - 1;
+            break;
+        case "ArrowDown":
+            newY = horizontalWalls.filter((wall) => wall[0] === friend.x)
+                .filter((wall) => wall[1] > friend.y)
+                .map((wall) => wall[1])
+                .sort()
+                .at(0);
+            friend.y = newY || GRID_SIZE - 1;
+            break;
+        case "ArrowLeft":
+            newY = horizontalWalls.filter((wall) => wall[1] === friend.y)
+                .filter((wall) => wall[1] <= friend.x)
+                .map((wall) => wall[1])
+                .sort()
+                .at(-1);
+            friend.x = newX || 0;
+            break;
+    }
 }
